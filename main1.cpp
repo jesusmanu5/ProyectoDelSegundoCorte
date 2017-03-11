f6b8623b3f31a5738a4d0b39875f5e95966f28bc
Unknown <96jmanuel@gmail.com>
2017-03-10 22:19:53 -0430

    Modificacion de Variables
    aca algunas limpiezas, aun falta terminar el menu

--- main1.cpp
+++ main1.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-#include "cant_vacas.h"
 #include <conio.h>
 #include <cstdlib>
+#include <string.h>
+#include <cctype>
+#include <sstream>
+#include "cant_vacas.h" /*inclui algunas librerias mas
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-char nombre;
-int peso;
-char raza;
-int litrosDeLecheDiario;
+char numero[15];
+char nomb[15];
+int contador[cant_vacas];
+string valortemp;
+float peso, lts_leche;
+string nombre;
+int raza=1;
+int op=0, cont=0; /*agrege nuevas variables */
+
 using namespace std;
 
 void agregar ();
@@ -50,20 +58,4 @@ int main() {
  getch();
  system("PAUSE");
  return 0;
-}
-
-int Cant_vacas[1000];
-	int i,j,aux;
-	
-//	for(i=0;i<1000;i++){
-//		for(j=0;j<1000;j++){
-//			if(array[j] > array[j+1]){
-//				aux = array[j];
-//				array[j] = array[j+1];
-//				array[j+1] = aux;
-//			}
-//		}
-//	}
-void agregar (int){
-int cantidad;
 }
